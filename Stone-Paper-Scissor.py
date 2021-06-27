e44b89eb7b74c858888df48c75b8e5d3bdffb96b
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-27 19:15:36 +0530

    Update and rename scissor_paper_rock.py to Stone-Paper-Scissor.py

--- Stone-Paper-Scissor.py
+++ Stone-Paper-Scissor.py
@@ -8,12 +8,12 @@ root.title("Rock Scissor Paper")
 root.configure(background="#9b59b6")
 
 # picture
-rock_img = ImageTk.PhotoImage(Image.open("rock-user.png"))
-paper_img = ImageTk.PhotoImage(Image.open("paper-user.png"))
-scissor_img = ImageTk.PhotoImage(Image.open("scissors-user.png"))
-rock_img_comp = ImageTk.PhotoImage(Image.open("rock.png"))
-paper_img_comp = ImageTk.PhotoImage(Image.open("paper.png"))
-scissor_img_comp = ImageTk.PhotoImage(Image.open("scissors.png"))
+rock_img = ImageTk.PhotoImage(Image.open(".png"))
+paper_img = ImageTk.PhotoImage(Image.open(".png"))
+scissor_img = ImageTk.PhotoImage(Image.open(".png"))
+rock_img_comp = ImageTk.PhotoImage(Image.open(".png"))
+paper_img_comp = ImageTk.PhotoImage(Image.open(".png"))
+scissor_img_comp = ImageTk.PhotoImage(Image.open(".png"))
 
 # insert picture
 user_label = Label(root, image=scissor_img, bg="#9b59b6")
